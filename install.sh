7f16f100116baee2449239a3124cd9ee4b15f3ef
Adil Hanney <adilhanney@disroot.org>
2024-09-14 00:55:38 +0100

    feat: add online install

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/bash
 
-if [ ! -f "upscaled_vlc.sh" ]; then
-  echo "Please run this script from the same directory as upscaled_vlc.sh"
-  exit 1
+PROJECT_DIR=$(pwd)
+if [ ! -f "$PROJECT_DIR/upscaled_vlc.sh" ]; then
+  echo "Installing from GitHub"
+  TMPDIR=$(mktemp -d)
+  git clone https://github.com/adil192/upscaled_vlc "$TMPDIR/upscaled_vlc"
+  PROJECT_DIR="$TMPDIR/upscaled_vlc"
+else
+  echo "Installing from local files"
 fi
 
-cp upscaled_vlc.sh ~/.local/bin/
-cp com.adilhanney.upscaled_vlc.desktop ~/.local/share/applications/
+cp "$PROJECT_DIR/upscaled_vlc.sh" ~/.local/bin/
+cp "$PROJECT_DIR/com.adilhanney.upscaled_vlc.desktop" ~/.local/share/applications/
 mkdir -p ~/.local/share/icons/hicolor/scalable/apps/
-cp com.adilhanney.upscaled_vlc.svg ~/.local/share/icons/hicolor/scalable/apps/
+cp "$PROJECT_DIR/com.adilhanney.upscaled_vlc.svg" ~/.local/share/icons/hicolor/scalable/apps/
+
+rm -rf "$TMPDIR"
